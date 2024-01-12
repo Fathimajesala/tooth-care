3f170887b0cfd2a4bfc0271badab2258dbe21afc
fathimajesala <fathimajesala11@gmail.com>
2024-01-12 13:49:54 +0530

    second commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+include __DIR__ . '/config.php';
+include __DIR__ . '/helpers/AppManager.php';
+
+//--------------TEST
+
+require_once __DIR__ . '/models/User.php';
+
+$userModel = new User();
+$users = $userModel->getAllActive();
+
+dd($users);
+
+// -----------------------------------------------
+
+
+// $sm = AppManager::getSM();
+// $username = $sm->getAttribute("username");
+
+// if (isset($username)) {
+
+header('location: views/admin/dashboard.php');
+
+// } else {
+//   header('location: views/auth/login.php');
+// }
